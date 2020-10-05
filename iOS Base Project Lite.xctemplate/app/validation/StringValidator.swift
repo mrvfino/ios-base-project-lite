9b17c20e4614405ec3c02afd83968b634725f5bc
Mark Alain D. Rufino <mark.rufino.io@gmail.com>
2020-10-06 01:02:20 +0800

    adjust spaces

--- iOS Base Project Lite.xctemplate/app/validation/StringValidator.swift
+++ iOS Base Project Lite.xctemplate/app/validation/StringValidator.swift
@@ -3,14 +3,17 @@
 import Foundation
 
 protocol StringValidatorRule {
+    
     var message: String { get set }
+    
     func validate(value: String) -> Bool
+    
 }
 
 class StringValidator {
     
     var rules: [StringValidatorRule]
-    
+
     init(rules: [StringValidatorRule]) {
         self.rules = rules
     }
